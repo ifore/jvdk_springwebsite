bc22990ac5196fe30ad8a423919bf1c31ba43c04
vertelek <vitaliiandrusiak@gmail.com>
2018-03-24 19:29:11 +0100

    add new console output for Vanichka!

--- src/main/java/site/AppLaunch.java
+++ src/main/java/site/AppLaunch.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AppLaunch {
     public static void main(String[] args) {
+        System.out.println("start here...");
         SpringApplication.run(AppLaunch.class, args);
     }
 }
