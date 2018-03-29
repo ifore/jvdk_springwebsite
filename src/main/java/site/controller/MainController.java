093b7050ccb7a932f8c10065327f1a61a0982c5f
Ivan <androidvasa@gmail.com>
2018-03-29 17:51:59 +0200

    Added login through Steam

--- src/main/java/site/controller/MainController.java
+++ src/main/java/site/controller/MainController.java
@@ -1,13 +1,23 @@
 package site.controller;
 
+
+import org.apache.catalina.connector.Request;
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import org.springframework.web.servlet.ModelAndView;
+import site.configuration.SteamOpenID;
 import site.model.Article;
 import site.service.ArticleService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 
 @Controller
@@ -16,6 +26,7 @@ public class MainController {
     //Віжкриваєм доступ до БД
     @Autowired
     private ArticleService service;
+    private SteamOpenID so = new SteamOpenID();
 
     //Передає дані (дані: service.getAll для поля articles) шаблонізатору (всі статті з БД)
     //і відображає main.html користовачу
@@ -39,8 +50,23 @@ public class MainController {
         return "redirect:../";
     }
 
-    @RequestMapping(value = "/login")
-    public String loginPage() {
-        return "login";
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView method() {
+        return new ModelAndView("redirect:" + so.login("http://localhost:8080/auth"));
+
+    }
+
+    @RequestMapping(value = "/auth", method = RequestMethod.GET)
+    public ModelAndView dosomething(HttpServletRequest request, HttpServletResponse response)  throws IOException {
+
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("redirect:../");
+        String user = so.verify(request.getRequestURI(), request.getParameterMap());
+        if(user == null) {
+            return new ModelAndView("redirect:" + so.login("http://localhost:8080"));
+        }
+        request.getSession(true).setAttribute("steamid", user);
+        System.out.println("USER information" + user);
+        return new ModelAndView("redirect:" + so.login("http://localhost:8080"));
     }
 }
